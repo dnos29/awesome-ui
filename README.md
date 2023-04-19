daa35ad9043a5502ed966a874a5ad382db0a7b86
Sotatek-MattDang <son.dangngoc@setel.com>
2023-04-19 09:03:01 +0700

    init pug, scss

---
+++ README.md
@@ -0,0 +1,2 @@
+# clevr-html
+This project is for create html pages for lotuscommunity base on [Clevr design] (https://www.figma.com/file/4CV21lM5cOrONXAte0oeiG/Clevr_BookStoreWebsite_AllPage?node-id=2-2&t=KaWO9vJU1tn8qSdV-0) 
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, watch } = require('gulp');
+const pug = require('gulp-pug');
+
+const PUG_URL = 'pug/*.pug';
+const SCSS_URL = 'scss/*.scss';
+
+const sass = require('gulp-sass')(require('sass'));
+function css() {
+	return src(SCSS_URL)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(dest('css'));
+}
+exports.css = css;
+
+function html() {
+	return src(PUG_URL)
+		.pipe(pug({ pretty: true }))
+		.pipe(dest('html'));
+}
+exports.html = html;
+
+exports.watch = function () {
+	watch(SCSS_URL, css);
+	watch(PUG_URL, html);
+};
+// function defaultTasks(cb) {
+// 	console.log('Gulp works');
+// 	cb();
+// }
+
+// exports.default = defaultTasks;
